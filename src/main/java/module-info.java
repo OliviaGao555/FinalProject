fce0f6d7368ac4e522ca2845165a8b39209cce74
Yan Ren <ryan.ren3@gmail.com>
2024-10-02 20:20:21 -0400

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.quizapp.quizapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.quizapp.quizapp to javafx.fxml;
+    exports com.quizapp.quizapp;
+}
