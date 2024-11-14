bf9ed21511175d61bdd6286866725271075a1334
JiangaiGao <6249771@edu.vaniercollege.qc.ca>
2024-11-14 01:29:07 -0500

    Shuffle method for short answer questions. comments

--- src/main/java/com/quizapp/quizapp/QuestionGenerator.java
+++ src/main/java/com/quizapp/quizapp/QuestionGenerator.java
@@ -1,50 +1,31 @@
 package com.quizapp.quizapp;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class QuestionGenerator {
     /**
-     * Adds a wind multiple choice question into the questions that will be displayed to the user.
-     * @param questions the ArrayList of all questions.
-     */
-    public static void windQuestion(List<Question> questions) {
-        ArrayList<String> multiChoiceData = multiChoiWind();
-        String correctAnswer = multiChoiceData.get(0);
-        String[] options = {
-                multiChoiceData.get(0),
-                multiChoiceData.get(1),
-                multiChoiceData.get(2),
-                multiChoiceData.get(3)
-        };
-        // Shuffle the options
-        String[] shuffledOptions = shuffleOptions(options);
-        String questionText = multiChoiceData.get(4);
-        String help = multiChoiceData.get(5);
-        // Add the question to the list
-        questions.add(new MultipleChoiceQuestion(questionText, shuffledOptions, correctAnswer, help));
-    }
-
-    /**
      * Forms a type of multiple choice question about wind on ocean.
      * @return ret A String ArrayList of a few Strings needed to create this multiple choice question.
      */
-    public static ArrayList<String> multiChoiWind() {
+    public static ArrayList<String> multiChoiceWind() {
+        // Create random numbers for the two variables of the question.
         double randomW = Double.parseDouble(new DecimalFormat("#.##").format(ThreadLocalRandom.current().nextDouble(1.00,10.00)).replace(",","."));
         double randomS = Double.parseDouble(new DecimalFormat("#.##").format(ThreadLocalRandom.current().nextDouble(0.10,5.00)).replace(",","."));
+        // Choose two random values for the two variables.
         double wave = randomW;
         double speed = randomS;
+        // Set up the question.
         String questionText = String.format("Wind gusts create ripples on the ocean " +
                 "that have a wavelength of %,.2f cm and propagate at %,.2f m/s. What is their frequency?", wave, speed);
+        // Set up the correct answer, and the three incorrect choices.
         double correctAnswer = speed / (wave / 100);
         double randomAnswer1 = Double.parseDouble(new DecimalFormat("#.##").format(ThreadLocalRandom.current().nextDouble(1.00,500.00)).replace(",","."));
         double randomAnswer2 = Double.parseDouble(new DecimalFormat("#.##").format(ThreadLocalRandom.current().nextDouble(1.00,500.00)).replace(",","."));
         double randomAnswer3 = Double.parseDouble(new DecimalFormat("#.##").format(ThreadLocalRandom.current().nextDouble(1.00,500.00)).replace(",","."));
         String answer = String.format("%,.2f", correctAnswer);
+        // Set up help to show the steps and answer of the question.
         String help = String.format("The frequency of a wave can be calculated using the formula:\n" + "f = v/λ\n" +
                 "Where: f is the frequency, v is the velocity of the wave, and λ is the wavelength of the wave.\n\n" +
                 "Given that the velocity (v) of the wave is %,.2f m/s and the wavelength (λ) is %,.2f cm, " +
@@ -52,6 +33,7 @@ public class QuestionGenerator {
                 "f = (%,.2f m/s)/(%,.2f cm)\n" +
                 "Don't forget to convert the unit of the wavelength! (100cm = 1m)\n\n" +
                 "So, the frequency of the ripples on the ocean is %,.2f Hz.", speed, wave, speed, wave, correctAnswer);
+        // Set up the return value for this method, including all needed values to create a wind question.
         ArrayList<String> ret = new ArrayList<>();
         ret.add(answer + " Hz");
         ret.add(randomAnswer1 + " Hz");
@@ -60,6 +42,43 @@ public class QuestionGenerator {
         ret.add(questionText);
         ret.add(help);
         return ret;
+    }
+
+    /**
+     * Adds a wind multiple choice question into the questions that will be displayed to the user.
+     * @param questions the ArrayList of all questions.
+     */
+    public static void windQuestion(List<Question> questions) {
+        // Bring the values collected from multiChoiceWind().
+        ArrayList<String> multiChoiceData = multiChoiceWind();
+        // Separate each value.
+        String correctAnswer = multiChoiceData.get(0);
+        String questionText = multiChoiceData.get(4);
+        String help = multiChoiceData.get(5);
+        String[] options = {
+                multiChoiceData.get(0),
+                multiChoiceData.get(1),
+                multiChoiceData.get(2),
+                multiChoiceData.get(3)
+        };
+        // Shuffle the options.
+        String[] shuffledOptions = shuffleOptions(options);
+        // Add the question to the list.
+        questions.add(new MultipleChoiceQuestion(questionText, shuffledOptions, correctAnswer, help));
+    }
+
+    public static void termsQuestion(List<Question> questions) {
+        Map<String, String> terms = new HashMap<>();
+        terms.put("What is the term for the maximum displacement of an object from its equilibrium position in simple harmonic motion?", "Amplitude");
+        terms.put("What is the term for the number of oscillations per unit time?", "Frequency");
+        terms.put("What is the term for the time to complete a cycle?", "Period");
+        ArrayList<List<String>> shuffledQAndA = shuffleShorts(terms);
+        List<String> question = shuffledQAndA.get(0);
+        List<String> correctAnswer = shuffledQAndA.get(1);
+        String help = "- The maximum displacement from equilibrium is often denoted by the letter 'A'.\n" +
+                "- The number of oscillations per second is measured in Hertz (Hz).\n" +
+                "- The time to complete one full cycle of motion is the inverse of frequency.";
+        questions.add(new ShortAnswerQuestion(question, correctAnswer, help));
     }
 
     /**
@@ -75,5 +94,23 @@ public class QuestionGenerator {
             shuffledOptions[i] = shuffle.get(i);
         }
         return shuffledOptions;
+    }
+
+    public static ArrayList<List<String>> shuffleShorts(Map<String, String> qAndA) {
+        List<Map.Entry<String, String>> shuffle = new ArrayList<>(qAndA.entrySet());
+        Collections.shuffle(shuffle);
+
+        List<String> shuffledQ = new ArrayList<>();
+        for (Map.Entry<String, String> shuffledQAndA : shuffle) {
+            shuffledQ.add(shuffledQAndA.getKey());
+        }
+        List<String> shuffledA = new ArrayList<>();
+        for (Map.Entry<String, String> shuffledQAndA : shuffle) {
+            shuffledA.add(shuffledQAndA.getValue());
+        }
+        ArrayList<List<String>> shuffledQAndA = new ArrayList<>();
+        shuffledQAndA.add(shuffledQ);
+        shuffledQAndA.add(shuffledA);
+        return shuffledQAndA;
     }
 }
--- src/main/java/com/quizapp/quizapp/QuizApp.java
+++ src/main/java/com/quizapp/quizapp/QuizApp.java
@@ -13,15 +13,14 @@ import javafx.util.Duration;
 import java.util.*;
 
 public class QuizApp extends Application {
-    //Variables, big components of the application.
+    // Variables, big components of the application.
     private BorderPane root;
     private VBox commonSection;
     private GridPane questionSection;
-    //Variables, used to present questions.
+    // Variables, used to present questions.
     public List<Question> questions;
-    private Label questionCounterLabel;
     private int currentQuestionIndex = 0;
-    //Variables, used for the timer.
+    // Variables, used for the timer.
     private Timeline timer;
     private int timeLimit = 180;
     private Label timerLabel;
@@ -32,66 +31,60 @@ public class QuizApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Set up the root.
-        root = new BorderPane();
-        // Set up the common section with buttons
-        commonSection = new VBox();
+        // Create the buttons.
         Button submitButton = new Button("Submit");
         Button helpButton = new Button("Help");
         Button previousButton = new Button("Previous Question");
         Button nextButton = new Button("Next Question");
-        HBox hButtons = new HBox(20, submitButton, helpButton, previousButton, nextButton);
-        questionCounterLabel = new Label();
-        commonSection.getChildren().addAll(hButtons, questionCounterLabel);
-        commonSection.setSpacing(10);
-        commonSection.setPadding(new Insets(20));
-
-        // Initialize timer label and add it to the common section
+        HBox hButtons = new HBox(30, submitButton, helpButton, previousButton, nextButton);
+        // Set up timer label.
         timerLabel = new Label("Time left: " + timeLimit + " seconds");
         timerLabel.setStyle("-fx-text-fill: #addea6;");
+
+        // Set up the common section.
+        commonSection = new VBox();
+        commonSection.getChildren().add(hButtons);
         commonSection.getChildren().add(timerLabel);
-
+        commonSection.setSpacing(10);   // Space between timer & buttons
+        commonSection.setPadding(new Insets(20));   // Space wrapped around the common section
         commonSection.setPrefHeight(100);
+        // Set up the question section.
+        questionSection = new GridPane();
+        questionSection.setPadding(new Insets(20));   // Space wrapped around the question section
 
+        // Set up the root.
+        root = new BorderPane();
+        root.setCenter(questionSection);
         root.setBottom(commonSection);
 
-        // Set up the question section
-        questionSection = new GridPane();
-        questionSection.setPadding(new Insets(10));
-        root.setCenter(questionSection);
-
-        // Initialize questions
+        // Initialize questions and display the first question.
         initializeQuestions();
-
-        // Display the first question
         displayQuestion(currentQuestionIndex);
 
-        // Set up button actions
+        // Set up button actions.
         submitButton.setOnAction(e -> {
             Question currentQuestion = questions.get(currentQuestionIndex);
             boolean isCorrect = currentQuestion.isAnswerCorrect();
-            currentQuestion.showResult(isCorrect); // Show the result in the question section
+            currentQuestion.showResult(isCorrect);
         });
-
         nextButton.setOnAction(e -> {
             if (currentQuestionIndex < questions.size() - 1) {
                 currentQuestionIndex++;
                 displayQuestion(currentQuestionIndex);
             }
         });
-
         previousButton.setOnAction(e -> {
             if (currentQuestionIndex > 0) {
                 currentQuestionIndex--;
                 displayQuestion(currentQuestionIndex);
             }
         });
-
         helpButton.setOnAction(e -> {
             Question currentQuestion = questions.get(currentQuestionIndex);
-            currentQuestion.showHelp(); // Display the hint in the resultLabel
+            currentQuestion.showHelp();
         });
 
+        // Set up the scene.
         Scene scene = new Scene(root, 1132, 700);
         scene.getStylesheets().add("style.css");
         primaryStage.setScene(scene);
@@ -101,35 +94,31 @@ public class QuizApp extends Application {
 
     // Method to initialize a list of questions
     private void initializeQuestions() {
+        // Create an ArrayList of questions.
         questions = new ArrayList<>();
 
-        // Q1
+        // Q1.
         QuestionGenerator.windQuestion(questions);
 
-        // Q2
+        // Q2.
         QuestionGenerator.windQuestion(questions);
 
-        // example to show how to add short answer question
-        questions.add(new ShortAnswerQuestion(
-                List.of("What is the capital of France?", "Largest planet?", "H2O is the chemical formula for?"),
-                List.of("Paris", "Jupiter", "Water"),
-                "Some hint"
-        ));
+        // Q3.
+        QuestionGenerator.termsQuestion(questions);
+//        questions.add(new ShortAnswerQuestion(
+//                List.of("What is the term for the maximum displacement of an object from its equilibrium position in simple harmonic motion?",
+//                        "What is the term for the number of oscillations per unit time",
+//                        "What is the term for the time to complete a cycle"),
+//                List.of("Amplitude",
+//                        "Frequency",
+//                        "Period"),
+//                "Hint:\n" +
+//                        "- The maximum displacement from equilibrium is often denoted by the letter 'A'.\n" +
+//                        "- The number of oscillations per second is measured in Hertz (Hz).\n" +
+//                        "- The time to complete one full cycle of motion is the inverse of frequency."
+//        ));
 
-        questions.add(new ShortAnswerQuestion(
-                List.of("What is the term for the maximum displacement of an object from its equilibrium position in simple harmonic motion?",
-                        "What is the term for the number of oscillations per unit time",
-                        "What is the term for the time to complete a cycle"),
-                List.of("Amplitude",
-                        "Frequency",
-                        "Period"),
-                "Hint:\n" +
-                        "- The maximum displacement from equilibrium is often denoted by the letter 'A'.\n" +
-                        "- The number of oscillations per second is measured in Hertz (Hz).\n" +
-                        "- The time to complete one full cycle of motion is the inverse of frequency."
-        ));
-
-        // Shuffle the list to randomize the question order
+        // Shuffle the list to randomize the question order.
         Collections.shuffle(questions);
     }
 
